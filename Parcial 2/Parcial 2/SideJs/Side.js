992fb3591215ed1363d2a432097b0f63ab339600
Romo_Guillermo <l19100251@nlaredo.tecnm.mx>
2022-10-20 11:58:07 -0500

    cambios

---
+++ Parcial 2/Parcial 2/SideJs/Side.js
@@ -0,0 +1,5 @@
+window.onload=function() {
+    document.getElementById("BotonMenu").addEventListener("click", function() {   
+    document.getElementsById("barralab").classList.toggle("escondido");
+    })
+}
