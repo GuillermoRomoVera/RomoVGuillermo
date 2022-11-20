c09b7147bb9164e7366c490ab67527ba2b1945fd
Romo_Guillermo <l19100251@nlaredo.tecnm.mx>
2022-11-19 21:10:47 -0600

    cambios

---
+++ Parcial 3/Bootstrap copy/GetRegistro.php
@@ -0,0 +1,18 @@
+<?php
+
+$registro["IDCliente"] = "19100251";
+$registro["Nombre"] = "Guillermo Romo";
+$registro["Usuario"] = "Memo";
+$registro["Contraseña"] = "Memingo02";
+$registro["RFC"] = "02102001";
+$registro["Estado"] = "Tamaulipas";
+$registro["NIP"] = "2001";
+$registro["CodigoPostal"] = "88283";
+$registro["Ciudad"] = "Nuevo Laredo";
+
+
+echo json_encode($registro);
+
+
+
+?>
