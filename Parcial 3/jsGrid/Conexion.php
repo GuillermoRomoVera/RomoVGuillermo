dc60c6d16f0e944e74ea95c7057af2e7609e99ae
Romo_Guillermo <l19100251@nlaredo.tecnm.mx>
2022-11-24 20:39:58 -0600

    cambios

--- Parcial 3/jsGrid/Conexion.php
+++ Parcial 3/jsGrid/Conexion.php
@@ -11,7 +11,7 @@ $consulta="SELECT * FROM registrodatos.registro";
 //echo $consulta;
 $registros= mysqli_query($con,$consulta) or die("problemas");
 
-//$result = mysqli_fetch_array($registros,MYSQLI_ASSOC);
+$result = mysqli_fetch_all($registros,MYSQLI_ASSOC);
 mysqli_close($con);
 echo json_encode($result);
 
---
+++ Parcial 3/jsGrid/jsGrid.js
@@ -0,0 +1,41 @@
+    $(document).ready(function() {   
+    const settings = {
+        "async": true,
+        "cossDomain": true,
+        "url": "http://localhost:8081/RomoVGuillermo/Parcial%203/jsGrid/Conexion.php",
+        "method": "GET",
+        "headers": {
+        "Accept": "*/*"
+        }
+    }
+ 
+    $.ajax(settings).done(function (response){
+    let tabla =JSON.parse(response);
+
+    $("#jsGrid").jsGrid({
+        width: "100%",
+        height: "400px",
+ 
+        inserting: true,
+        editing: true,
+        sorting: true,
+        paging: true,
+
+        data: tabla,
+ 
+
+        fields: [
+            { name: "Clave", type: "number", textField:"Clave"},
+            { name: "Cliente", type: "text", textField:"Cliente"},
+            { name: "Ciudad", type: "text", textField:"Ciudad"},
+            { name: "Estado", type: "text", textField:"Estado"},
+            { name: "CodigoPostal", type: "number", textField:"CodigoPostal"},
+            { name: "Usuario", type: "text", textField:"Usuario"},
+            { name: "RFC", type: "text", textField:"RFC"},
+            { name: "Contrasena", type: "text", textField:"Contrasena"},
+            { name: "IDCliente", type: "number", textField:"IDCliente"},
+
+        ]
+      });
+    });
+});
