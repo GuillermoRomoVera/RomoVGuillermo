2429c9d4942b748949e0fff145c6e7f7a410917a
Romo_Guillermo <l19100251@nlaredo.tecnm.mx>
2022-11-29 19:35:44 -0600

    cambios

--- Parcial 3/Grid/Conexion.php
+++ Parcial 3/Grid/Conexion.php
@@ -11,7 +11,7 @@ $consulta="SELECT * FROM registrodatos.registro";
 //echo $consulta;
 $registros= mysqli_query($con,$consulta) or die("problemas");
 
-$result = mysqli_fetch_all($registros);
+$result = mysqli_fetch_all($registros,MYSQLI_ASSOC);
 mysqli_close($con);
 echo json_encode($result);
 
